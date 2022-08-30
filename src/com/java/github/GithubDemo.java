3d56435463ead01e431620019b82927fe998cab2
plakshmi <pasumarthy.lakshmi@soprasteria.com>
2022-08-30 12:11:27 +0530

    java project commit (first)

--- src/com/java/github/GithubDemo.java
+++ src/com/java/github/GithubDemo.java
@@ -2,8 +2,9 @@ package com.java.github;
 
 public class GithubDemo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("Git hub demo");//right click on project goto team ->share project->click on check box and then click on create repository,then again right 
 
 	}
 
