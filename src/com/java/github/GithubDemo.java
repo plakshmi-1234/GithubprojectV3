c9cf5928edf38be6ec719c0ee565d37b72a30e2b
plakshmi <pasumarthy.lakshmi@soprasteria.com>
2022-08-30 12:50:52 +0530

    java project commit (first commit)

--- src/com/java/github/GithubDemo.java
+++ src/com/java/github/GithubDemo.java
@@ -4,7 +4,7 @@ public class GithubDemo {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Git hub demo");//right click on project goto team ->share project->click on check box and then click on create repository,then again right 
+		System.out.println("Git hub demo");//right click on project goto team ->share project->click on check box and then click on create repository,then again right click click on team->add to index->again right click team->coomit 
 
 	}
 
